dcf16fdbd2fba3ab41ecccc1220c1b5059b2b06f
DanielBrisch <danielbrisch157@gmail.com>
2024-09-29 16:44:30 -0300

    feat: adicionando testes unitarios e algumas refatoracoes no codigo

--- app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
@@ -34,33 +34,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calcular() {
+        if (!verificaCamposVazios()) {
+            return;
+        }
+
+        val imc = Calculo()
+        val result = imc.calculaIMC(valorPeso.text.toString().toDouble(), valorAltura.text.toString().toDouble(), "en")
+
+        valorImc.text = result.toString()
+    }
+
+    private fun verificaCamposVazios() : Boolean {
         if (valorPeso.text.toString().isEmpty()) {
             valorPeso.error = getString(R.string.campo_vazio)
             valorPeso.requestFocus()
-            return
+            return false
         }
 
         if (valorAltura.text.toString().isEmpty()) {
             valorAltura.error = getString(R.string.campo_vazio)
             valorAltura.requestFocus()
-            return
+            return false
         }
-
-        val peso = valorPeso.text.toString().toDouble()
-        val altura = valorAltura.text.toString().toDouble()
-
-        var result = 0.0
-        lateinit var numbarFormat : NumberFormat
-
-        if (Locale.getDefault().language.equals("en")) {
-            result = 703 * (peso / altura.pow(2))
-            numbarFormat = NumberFormat.getNumberInstance(Locale.US)
-        } else {
-            result = peso / altura.pow(2)
-            numbarFormat = NumberFormat.getNumberInstance(Locale.getDefault())
-        }
-
-        valorImc.text = numbarFormat.format(result)
+        return true
     }
 
     private fun limpar() {
@@ -77,5 +73,26 @@ class MainActivity : AppCompatActivity() {
         btnCalcular = findViewById(R.id.btnCalcular)
         btnLimpar = findViewById(R.id.btnLimpar)
         valorImc = findViewById(R.id.valorImc)
+    }
+}
+
+class Calculo {
+
+    fun calculaIMC(peso: Double, altura: Double, locale: String): Double {
+        var result = 0.0
+
+        if (locale == "en")  {
+            result = peso / altura.pow(2)
+        } else if (locale == "pt") {
+            result = 703 * (peso / altura.pow(2))
+        }
+
+        val numberFormat: NumberFormat = when (locale) {
+            "en" -> NumberFormat.getNumberInstance(Locale.US)
+            "pt" -> NumberFormat.getNumberInstance(Locale("pt", "BR"))
+            else -> NumberFormat.getNumberInstance(Locale.getDefault())
+        }
+
+        return numberFormat.format(result).toDouble()
     }
 }
--- app/src/test/java/br/edu/utfpr/calculaimc_kotlin/ExampleUnitTest.kt
+++ app/src/test/java/br/edu/utfpr/calculaimc_kotlin/ExampleUnitTest.kt
@@ -4,14 +4,10 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun calculaImcBrazil() {
+        val imc = Calculo()
+        assertTrue("IMC calculado: ", imc.calculaIMC(80.0, 1.80, "pt") in 24.6 .. 24.7)
     }
 }
