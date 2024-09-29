7b3e226b3320b1aa1a9678954d90d35d582cbf01
DanielBrisch <danielbrisch157@gmail.com>
2024-09-29 15:49:01 -0300

    feat: adicionando internacionalizacao

--- app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import java.text.NumberFormat
+import java.util.Locale
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -33,29 +35,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun calcular() {
         if (valorPeso.text.toString().isEmpty()) {
-            valorPeso.error = "Campo deve ser preenchido"
+            valorPeso.error = getString(R.string.campo_vazio)
             valorPeso.requestFocus()
             return
         }
 
         if (valorAltura.text.toString().isEmpty()) {
-            valorAltura.error = "Campo deve ser preenchido"
+            valorAltura.error = getString(R.string.campo_vazio)
             valorAltura.requestFocus()
             return
         }
 
         val peso = valorPeso.text.toString().toDouble()
         val altura = valorAltura.text.toString().toDouble()
-        val imc = peso / altura.pow(2)
-        valorImc.text = "%.2f".format(imc)
+
+        var result = 0.0
+        lateinit var numbarFormat : NumberFormat
+
+        if (Locale.getDefault().language.equals("en")) {
+            result = 703 * (peso / altura.pow(2))
+            numbarFormat = NumberFormat.getNumberInstance(Locale.US)
+        } else {
+            result = peso / altura.pow(2)
+            numbarFormat = NumberFormat.getNumberInstance(Locale.getDefault())
+        }
+
+        valorImc.text = numbarFormat.format(result)
     }
 
     private fun limpar() {
         valorPeso.setText("")
         valorAltura.setText("")
-        valorImc.setText("0.0")
+        valorImc.text = getString(R.string.hint_imc)
         valorPeso.requestFocus()
-        Toast.makeText(this, "Tela reiniciada", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, getString(R.string.toas_limpar), Toast.LENGTH_LONG).show()
     }
 
     private fun iniciaComponentes() {
